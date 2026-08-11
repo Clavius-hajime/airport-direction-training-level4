f949b3360edc97cef6f5634acee41ce76ff004fa
Clavius-hajime <hajime.sogawa@gmail.com>
2026-08-11 16:17:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='airport-direction-training-level4-v7';
+const CACHE_NAME='airport-direction-training-level4-v8';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./airport-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
