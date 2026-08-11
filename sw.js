bd889ce35390b3202ed4e6c5ef459fa813974c90
Clavius-hajime <hajime.sogawa@gmail.com>
2026-08-11 17:15:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='airport-direction-training-level4-v11';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./airport-icon.png'];
+const CACHE_NAME = 'airport-direction-training-level4-v12';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./airport-icon.png', './apple-touch-icon.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE_NAME).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
